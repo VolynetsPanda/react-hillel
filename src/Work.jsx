864484be7c3d8857e34678255ba2ff66067140b6
VolynetsPanda <hw.volynets@gmail.com>
2021-10-10 14:25:15 +0300

    lessons-3

--- src/Work.jsx
+++ src/Work.jsx
@@ -10,12 +10,15 @@ class Work extends React.Component{
             likes: 0,
             move: 0,
             step: 10,
-            text: '',
-            pass: ''
+            text: 'on',
+            pass: '',
+            color: 'black',
+            products: []
         }
         this.changeLikes = this.changeLikes.bind(this);
         this.onMove = this.onMove.bind(this);
         this.changeText = this.changeText.bind(this);
+        this.press = this.press.bind(this);
 
     }
     changeLikes(step){
@@ -29,30 +32,59 @@ class Work extends React.Component{
             move: move + step
         })
     }
+    press(){
+        // this.setState({
+        //     text: this.state.text === "on" ? 'off' : 'on'
+        // })
+        this.forceUpdate()
+    }
     changeText(e, key){
         this.setState({
             [key]: e.target.value
         })
     }
-    // changePass(e){
-    //     this.setState({
-    //         pass: e.target.value
-    //     })
-    // }
+    componentDidMount() {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+                console.log(json)
+                this.setState({
+                    products: json
+                })
+            })
+    }
+    static getDerivedStateFromProps(state, props){
+        console.log('getDerivedStateFromProps', state, props)
+        return null
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', prevState, this.state);
+        return  prevState.text !== this.state.text
+        // return true
+    }
+
     render() {
-        const {pass, text} = this.state;
+        console.log('render')
+        const {pass, text, color, products} = this.state;
         return (
             <>
+                {products.length ? products.map((item)=>
+                    <div key={item.id}>
+                        <h3>{item.title}</h3>
+                        <a href="#">Add</a>
+                    </div>) : <p>Loading...</p>
+                }
+                {/*<input type={'text'} value={text} />*/}
                 {/*<Block move={move} />*/}
-                <DefaultInput
-                    type={'text'}
-                    change={(e) => this.changeText(e, 'text')}
-                />
-                <DefaultInput
-                    type={'password'}
-                    change={(e) => this.changeText(e, 'pass')}
-                />
-                <button onClick={this.onMove}>touch me!</button>
+                {/*<DefaultInput*/}
+                {/*    type={'text'}*/}
+                {/*    change={(e) => this.changeText(e, 'text')}*/}
+                {/*/>*/}
+                {/*<DefaultInput*/}
+                {/*    type={'password'}*/}
+                {/*    change={(e) => this.changeText(e, 'pass')}*/}
+                {/*/>*/}
+                {/*<button onClick={this.press} style={{color: color}}>{text}</button>*/}
                 {/*{users?.map((user) =>*/}
                 {/*    <Test*/}
                 {/*        user={user}*/}
